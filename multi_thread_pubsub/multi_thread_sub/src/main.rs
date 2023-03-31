2b5d05a8b831af043eec2adabf03c6c02047f645
Tatsuhiro Ikebe <beike315@icloud.com>
2023-04-01 01:27:23 +0900

    Feature multi thread pubsub (#2)
    * Feature multi thread pub
    
    * Feature multi thread sub
    
    * Feature multi thread pubsub

---
+++ multi_thread_pubsub/multi_thread_sub/src/main.rs
@@ -0,0 +1,29 @@
+use safe_drive::{
+  context::Context, error::DynError, logger::Logger, pr_info, topic::subscriber::Subscriber,
+};
+
+#[async_std::main]
+async fn main() -> Result<(), DynError> {
+  let ctx = Context::new()?;
+  let node = ctx.create_node("multi_thread_sub", None, Default::default())?;
+
+  let subscriber1 = node.create_subscriber::<std_msgs::msg::String>("multi_thread_pubsub_1", None)?;
+  let subscriber2 = node.create_subscriber::<std_msgs::msg::String>("multi_thread_pubsub_2", None)?;
+
+  let task1 = async_std::task::spawn(receiver(subscriber1));
+  let task2 = async_std::task::spawn(receiver(subscriber2));
+
+  task1.await?;
+  task2.await?;
+
+  Ok(())
+}
+
+async fn receiver(mut subscriber: Subscriber<std_msgs::msg::String>) -> Result<(), DynError> {
+  let logger = Logger::new(subscriber.get_topic_name());
+
+  loop {
+    let msg = subscriber.recv().await?;
+    pr_info!(logger, "{}", msg.data);
+  }
+}
