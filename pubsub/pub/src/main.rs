ed4051cd53567427e7d6567f000b6fe16f20626a
Tatsuhiro Ikebe <beike315@icloud.com>
2023-04-01 00:45:03 +0900

    Feature pubsub (#1)
    * Feature pub
    
    * Feature sub
    
    * Feature pubsub

---
+++ pubsub/pub/src/main.rs
@@ -0,0 +1,28 @@
+use safe_drive::{
+  context::Context, error::DynError, logger::Logger, pr_info
+};
+use std::time::Duration;
+
+fn main() -> Result<(), DynError> {
+  let ctx = Context::new()?;
+
+  let node = ctx.create_node("pub", None, Default::default())?;
+
+  let publisher = node.create_publisher::<std_msgs::msg::String>("pubsub", None)?;
+
+  let logger = Logger::new("pub");
+
+  let mut cnt = 0;
+  let mut msg = std_msgs::msg::String::new().unwrap();
+  loop {
+      let data = format!("Hello, World!: cnt = {cnt}");
+      msg.data.assign(&data);
+
+      pr_info!(logger, "send: {}", msg.data);
+
+      publisher.send(&msg)?;
+
+      cnt += 1;
+      std::thread::sleep(Duration::from_secs(1));
+  }
+}
