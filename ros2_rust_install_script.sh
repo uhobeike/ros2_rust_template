692dfb5d3892b89f194f425a92208bfe1420c795
Tatsuhiro Ikebe <beike315@icloud.com>
2023-04-04 23:16:55 +0900

    Feature github actions ci (#8)
    * Feature github actions ci
    
    * Fix sh path
    
    * Debug tree
    
    * Debug tree
    
    * Fix sh path
    
    * Fix ci path
    
    * Fix sh option -y
    
    * Fix sh option -y
    
    * Fix sh option
    
    * Fix sh option
    
    * Debug ssh
    
    * Add rm cargo-ament-build
    
    * Add source rust env
    
    * Fix sh path
    
    * Add ci badge
    
    * Fix ci yaml push branch

--- ros2_rust_install_script.sh
+++ ros2_rust_install_script.sh
@@ -2,11 +2,11 @@
 
 sudo apt install curl gnupg2 lsb-release python3-pip git
 
-sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
-sudo apt update
-sudo apt install ros-humble-desktop python3-colcon-common-extensions
+sudo apt update -y
+sudo apt install -y ros-humble-desktop python3-colcon-common-extensions
 
 pip install git+https://github.com/tier4/colcon-cargo.git
 pip install git+https://github.com/colcon/colcon-ros-cargo.git
@@ -16,3 +16,5 @@ pip install git+https://github.com/colcon/colcon-ros-cargo.git
 git clone https://github.com/tier4/cargo-ament-build.git
 cd cargo-ament-build
 cargo install --path .
+cd ..
+rm cargo-ament-build -r
