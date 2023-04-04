4af772bf40e3f385f0d695dd4bd1f2759fdb16d5
uhobeike <beike315@icloud.com>
2023-04-04 22:31:59 +0900

    Fix sh option

--- ros2_rust_install_script.sh
+++ ros2_rust_install_script.sh
@@ -2,14 +2,14 @@
 
 sudo apt install curl gnupg2 lsb-release python3-pip git
 
-sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -y | sh
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
-sudo apt update
-sudo apt install ros-humble-desktop python3-colcon-common-extensions
+sudo apt update -y
+sudo apt install -y ros-humble-desktop python3-colcon-common-extensions
 
-pip install git+https://github.com/tier4/colcon-cargo.git
-pip install git+https://github.com/colcon/colcon-ros-cargo.git
+pip install -y git+https://github.com/tier4/colcon-cargo.git
+pip install -y git+https://github.com/colcon/colcon-ros-cargo.git
 
 . /opt/ros/humble/setup.bash
 
