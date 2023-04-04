616a4ba1decd1ba1ea80288b19e3e6f55c56237d
Tatsuhiro Ikebe <beike315@icloud.com>
2023-04-04 21:33:39 +0900

    Format/first cargo format (#7)
    * Format code
    
    * Update README

--- parameter/parameter_multi_thread_sub/src/main.rs
+++ parameter/parameter_multi_thread_sub/src/main.rs
@@ -1,29 +1,29 @@
 use safe_drive::{
-  context::Context, error::DynError, logger::Logger, pr_info, topic::subscriber::Subscriber,
+    context::Context, error::DynError, logger::Logger, pr_info, topic::subscriber::Subscriber,
 };
 
 #[async_std::main]
 async fn main() -> Result<(), DynError> {
-  let ctx = Context::new()?;
-  let node = ctx.create_node("parameter_multi_thread_sub", None, Default::default())?;
+    let ctx = Context::new()?;
+    let node = ctx.create_node("parameter_multi_thread_sub", None, Default::default())?;
 
-  let subscriber1 = node.create_subscriber::<std_msgs::msg::String>("task_1", None)?;
-  let subscriber2 = node.create_subscriber::<std_msgs::msg::String>("task_2", None)?;
+    let subscriber1 = node.create_subscriber::<std_msgs::msg::String>("task_1", None)?;
+    let subscriber2 = node.create_subscriber::<std_msgs::msg::String>("task_2", None)?;
 
-  let task1 = async_std::task::spawn(receiver(subscriber1));
-  let task2 = async_std::task::spawn(receiver(subscriber2));
+    let task1 = async_std::task::spawn(receiver(subscriber1));
+    let task2 = async_std::task::spawn(receiver(subscriber2));
 
-  task1.await?;
-  task2.await?;
+    task1.await?;
+    task2.await?;
 
-  Ok(())
+    Ok(())
 }
 
 async fn receiver(mut subscriber: Subscriber<std_msgs::msg::String>) -> Result<(), DynError> {
-  let logger = Logger::new(subscriber.get_topic_name());
+    let logger = Logger::new(subscriber.get_topic_name());
 
-  loop {
-    let msg = subscriber.recv().await?;
-    pr_info!(logger, "{}", msg.data);
-  }
+    loop {
+        let msg = subscriber.recv().await?;
+        pr_info!(logger, "{}", msg.data);
+    }
 }
